ab74a072a38b5e12c53782b1b0a3f51e1f7267dc
Timothy Jennike <jenniketimothy00@gmail.com>
2023-03-13 13:37:56 +0200

    test6

--- backend/models/db.js
+++ backend/models/db.js
@@ -9,9 +9,29 @@ const connection = mysql.createConnection({
     port: database.Port
 });
 
-connection.connect(error => {
+connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + connection.threadId);
+  });
+  
+  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      connection.end(); // close the connection to the database
+      return;
+    }
+  
+    console.log('The solution is: ', results[0].solution);
+    connection.end(); // close the connection to the database
+  });
+
+// connection.connect(error => {
 //     if (error) throw error;
 //     console.log("Database is Connected uh..Successfully");
-});
+// });
 
 module.exports = connection
