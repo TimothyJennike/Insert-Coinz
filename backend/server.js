74af708fe431ee21e74d0b95d753ac850e6799e4
Timothy Jennike <jenniketimothy00@gmail.com>
2023-03-13 12:14:38 +0200

    test3

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:3300"
+    origin: "http://localhost:3306"
 };
 
 app.use(cors(corsOptions));
